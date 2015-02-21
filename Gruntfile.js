f4d5376a69be5e852e18a19d2786c8b905290a72
Stephen Thomas <stephen@sathomas.me>
2015-02-21 11:10:24 -0500

    remove global jshint rules (.jshintrc) to avoid linthub conflicts

--- Gruntfile.js
+++ Gruntfile.js
@@ -110,7 +110,6 @@ module.exports = function (grunt) {
         },
         jshint: {
             options: {
-                jshintrc: '.jshintrc',
                 reporter: require('jshint-stylish')
             },
             all: [
