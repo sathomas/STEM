1ad55d6422cb1cfd69f4dc19063af0f8bb169eed
Carroll, Jordan <Jordan.Carroll@gtri.gatech.edu>
2015-05-07 10:39:10 -0400

    view_unit_test: more tests

--- test/views/adminsAsDiscovery.spec.js
+++ test/views/adminsAsDiscovery.spec.js
@@ -28,10 +28,29 @@ describe('AdminsAsDiscovery View', function() {
         $el.find('h3').attr('id').should.match(/^[0-9]+$/);
     });
 
-    it('post render: should have a link to stemgeorgia roadmap', function() {
+    it('after render, should have a link to stemgeorgia roadmap', function() {
         var $el = this.AdminsAsDiscovery.render().$el;
 
-        var link = $el.find('.invite .btn');
+        $el.find('.invite .btn').length.should.equal(1);
         $el.find('.invite .btn').attr('href').should.equal('http://stemgeorgia.org/roadmap/');
+    });
+
+    it('after render, the map div should be a leaflet container', function() {
+        var $el = this.AdminsAsDiscovery.render().$el;
+
+        var mapContainer = $el.find('#admins-certifications-map');
+        mapContainer.length.should.equal(1);
+        mapContainer.hasClass('leaflet-container').should.be.true();
+    });
+
+    it('after render, should have link to show more spotlights', function() {
+        // var $el = this.AdminsAsDiscovery.render().$el;
+
+        // $el.find('admins-spotlights')
+        // var $el = this.OaeAsSecondarySearchGroup.render().$el;
+        // var redirectStub = sinon.stub(Stem.Utils, 'redirect');
+        // $el.find('button').click();
+        // redirectStub.calledWith('/more/link').should.be.true();
+        // redirectStub.restore();
     });
 });
