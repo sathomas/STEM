f0f87b92ccfcfe888a82a50f65e26c3437269ef0
Giuseppe Scoppino <gscoppino3@gtri.gatech.edu>
2015-05-18 14:12:27 -0400

    Slightly improved coverage of discoveryAsContent, and re-structuring to match other views and test more things.

--- test/views/discoveryAsContent.spec.js
+++ test/views/discoveryAsContent.spec.js
@@ -4,15 +4,11 @@ describe('DiscoveryAsContent View', function() {
     'use strict';
 
     beforeEach(function() {
-        this.$emptyScaffolding = $('<section id="landing"></section>');
-        this.$scaffolding = $('<section id="landing">' +
-                                '<article id="teachers_js" class="discovery"></article>' +
-                                '<article id="admins_js" class="discovery"></article>' +
-                                '<article id="partners_js" class="discovery"></article>' +
-                            '</section>');
+        this.$Scaffolding = $('<section id="landing"></section>');
+
         this.Discovery = new Stem.Models.Discovery();
         this.DiscoveryAsContent = new Stem.Views.DiscoveryAsContent({
-            el: this.$emptyScaffolding,
+            el: this.$Scaffolding,
             model: this.Discovery
         });
     });
@@ -23,61 +19,45 @@ describe('DiscoveryAsContent View', function() {
         });
 
         it('should fill in the provided element, if it was empty.', function() {
-            var emptyDiscovery = new Stem.Views.DiscoveryAsContent({
-                el: this.$emptyScaffolding,
-                model: this.Discovery
-            }).render();
-
-            expect(emptyDiscovery.$el.is(':empty')).to.be.false;
-        });
-
-        it('should fill in a scaffolding which already contains discovery articles.', function() {
             var $el = this.DiscoveryAsContent.render().$el;
-            $el.is(':empty').should.be.false();
+            $el.is(':empty').should.be.false;
+            $el.find('nav').length.should.not.equal(0);
+            $el.find('article#teachers_js').length.should.not.equal(0);
+            $el.find('article#admins_js').length.should.not.equal(0);
+            $el.find('article#partners_js').length.should.not.equal(0);
         });
     });
 
     describe('After Rendering', function() {
-        it('changing discovery radio buttons should navigate the page to the correct hash.', function() {
-            var $el = this.DiscoveryAsContent.render().$el;
-
-            var radioButtons = $el.find('.discovery-nav__radio');
-            radioButtons.length.should.equal(3);
-
-            var radioButton = radioButtons[1];
-            $(radioButton).click();
-
-            var discoveryNav = $el.find('#discovery-nav');
-            discoveryNav.on('change', function() {
-                console.log('changed!');
+        it('submission of a search should be passed up the view chain for handling.', function(done) {
+            var spy = sinon.spy(this.DiscoveryAsContent, "submitSearch");
+            this.DiscoveryAsContent.render();
+            this.DiscoveryAsContent.on('search:submit', function() {
+                done();
             });
 
-            discoveryNav.change();
+            this.DiscoveryAsContent.teacherDiscovery.trigger('search:submit');
+            spy.callCount.should.equal(1);
 
-            // var ev = document.createEvent("MouseEvent");
-            // ev.initMouseEvent(
-            //   "click",
-            //   true /* bubble */, true /* cancelable */,
-            //   window, null,
-            //   0, 0, 0, 0, /* coordinates */
-            //   false, false, false, false, /* modifier keys */
-            //   0 /*left*/, null
-            // );
-            // el.dispatchEvent(ev);
-            click(radioButton);
+            this.DiscoveryAsContent.off('search:submit');
+            spy.restore();
+        });
 
-            function click(el){
-                var ev = document.createEvent("MouseEvent");
-                ev.initMouseEvent(
-                    "click",
-                    true /* bubble */, true /* cancelable */,
-                    window, null,
-                    0, 0, 0, 0, /* coordinates */
-                    false, false, false, false, /* modifier keys */
-                    0 /*left*/, null
-                );
-                el.dispatchEvent(ev);
-            }
+        // Could not say why this test does not work in its current state;
+        // However, like TeacherSearchAsPage, it couldn't be tested by directly
+        // probing the radio buttons, so attempted to just trigger the navigation
+        // event manually.
+        it.skip('changing discovery radio buttons should navigate the page correctly.', function() {
+            var $el = this.DiscoveryAsContent.render().$el;
+
+            var $navigationElements = $el.find('#discovery-nav .discovery-nav__radio');
+            $navigationElements.length.should.equal(3);
+
+            var discovery_as_content = this.DiscoveryAsContent;
+            ['teachers_js', 'admins_js', 'partners_js'].forEach(function(hash) {
+                discovery_as_content.trigger('navigate', hash);
+                window.location.hash.should.equal('#' + hash.slice(0, -3));
+            });
         });
     });
 });
