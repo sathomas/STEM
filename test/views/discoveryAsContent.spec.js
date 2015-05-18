7cb86fc5019baa3177e592f2e44a3957c6a97902
Giuseppe Scoppino <gscoppino3@gtri.gatech.edu>
2015-05-18 12:02:39 -0400

    Change format of DiscoveryAsContent to match alternate test format.

--- test/views/discoveryAsContent.spec.js
+++ test/views/discoveryAsContent.spec.js
@@ -1,15 +1,15 @@
 /* global Stem, describe, beforeEach, it */
 
-describe('View::DiscoveryAsContent', function() {
+describe('DiscoveryAsContent View', function() {
     'use strict';
 
     beforeEach(function() {
         this.$emptyScaffolding = $('<section id="landing"></section>');
         this.$scaffolding = $('<section id="landing">' +
-                                        '<article id="teachers_js" class="discovery"></article>' +
-                                        '<article id="admins_js" class="discovery"></article>' +
-                                        '<article id="partners_js" class="discovery"></article>' +
-                                    '</section>');
+                                '<article id="teachers_js" class="discovery"></article>' +
+                                '<article id="admins_js" class="discovery"></article>' +
+                                '<article id="partners_js" class="discovery"></article>' +
+                            '</section>');
         this.Discovery = new Stem.Models.Discovery();
         this.DiscoveryAsContent = new Stem.Views.DiscoveryAsContent({
             el: this.$emptyScaffolding,
@@ -17,63 +17,67 @@ describe('View::DiscoveryAsContent', function() {
         });
     });
 
-    it('Render method should return the view (for chaining).', function() {
-        this.DiscoveryAsContent.render().should.equal(this.DiscoveryAsContent);
-    });
-
-    it('After render, if the provided el was empty, should no longer be empty.', function() {
-        var $el = this.DiscoveryAsContent.render().$el;
-        $el.is(':empty').should.be.false();
-    });
-
-    it('post render: if the element is empty, the template should be used to fill it in', function() {
-        var emptyDiscovery = new Stem.Views.DiscoveryAsContent({
-            el: this.$emptyScaffolding,
-            model: this.Discovery
-        }).render();
-
-        expect(emptyDiscovery.$el.is(':empty')).to.be.false;
-    });
-
-    it('post render: should navigate to the hash when discovery radio buttons are changed', function() {
-        var $el = this.DiscoveryAsContent.render().$el;
-
-        var radioButtons = $el.find('.discovery-nav__radio');
-        radioButtons.length.should.equal(3);
-
-        var radioButton = radioButtons[1];
-        $(radioButton).click();
-
-        var discoveryNav = $el.find('#discovery-nav');
-        discoveryNav.on('change', function() {
-            console.log('changed!');
+    describe('Rendering', function() {
+        it('should return the view (for chaining).', function() {
+            this.DiscoveryAsContent.render().should.equal(this.DiscoveryAsContent);
         });
 
-        discoveryNav.change();
+        it('should fill in the provided element, if it was empty.', function() {
+            var emptyDiscovery = new Stem.Views.DiscoveryAsContent({
+                el: this.$emptyScaffolding,
+                model: this.Discovery
+            }).render();
 
-        // var ev = document.createEvent("MouseEvent");
-        // ev.initMouseEvent(
-        //   "click",
-        //   true /* bubble */, true /* cancelable */,
-        //   window, null,
-        //   0, 0, 0, 0, /* coordinates */
-        //   false, false, false, false, /* modifier keys */
-        //   0 /*left*/, null
-        // );
-        // el.dispatchEvent(ev);
-        click(radioButton);
+            expect(emptyDiscovery.$el.is(':empty')).to.be.false;
+        });
 
-        function click(el){
-            var ev = document.createEvent("MouseEvent");
-            ev.initMouseEvent(
-                "click",
-                true /* bubble */, true /* cancelable */,
-                window, null,
-                0, 0, 0, 0, /* coordinates */
-                false, false, false, false, /* modifier keys */
-                0 /*left*/, null
-            );
-            el.dispatchEvent(ev);
-        }
+        it('should fill in a scaffolding which already contains discovery articles.', function() {
+            var $el = this.DiscoveryAsContent.render().$el;
+            $el.is(':empty').should.be.false();
+        });
+    });
+
+    describe('After Rendering', function() {
+        it('changing discovery radio buttons should navigate the page to the correct hash.', function() {
+            var $el = this.DiscoveryAsContent.render().$el;
+
+            var radioButtons = $el.find('.discovery-nav__radio');
+            radioButtons.length.should.equal(3);
+
+            var radioButton = radioButtons[1];
+            $(radioButton).click();
+
+            var discoveryNav = $el.find('#discovery-nav');
+            discoveryNav.on('change', function() {
+                console.log('changed!');
+            });
+
+            discoveryNav.change();
+
+            // var ev = document.createEvent("MouseEvent");
+            // ev.initMouseEvent(
+            //   "click",
+            //   true /* bubble */, true /* cancelable */,
+            //   window, null,
+            //   0, 0, 0, 0, /* coordinates */
+            //   false, false, false, false, /* modifier keys */
+            //   0 /*left*/, null
+            // );
+            // el.dispatchEvent(ev);
+            click(radioButton);
+
+            function click(el){
+                var ev = document.createEvent("MouseEvent");
+                ev.initMouseEvent(
+                    "click",
+                    true /* bubble */, true /* cancelable */,
+                    window, null,
+                    0, 0, 0, 0, /* coordinates */
+                    false, false, false, false, /* modifier keys */
+                    0 /*left*/, null
+                );
+                el.dispatchEvent(ev);
+            }
+        });
     });
 });
